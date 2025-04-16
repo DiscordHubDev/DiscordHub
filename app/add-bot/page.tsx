6e6f4d25b3b2c24fe1557e73e215b73a0b842456
victorydawn <setkillthe1@gmail.com>
2025-04-17 03:40:06 +0800

    設置新增機器人頁面的seo並優化搜尋和更多內容

--- app/add-bot/page.tsx
+++ app/add-bot/page.tsx
@@ -1,15 +1,25 @@
 'use client';
 
+import Head from 'next/head';
 import { submitBot } from '@/lib/actions/submit-bot';
 import { sendNotification } from '@/lib/actions/sendNotification';
 import { BotWithRelationsInput } from '@/lib/prisma_type';
-import {
-  BotFormData,
-  BotInfo,
-  DiscordBotRPCInfo,
-  Screenshot,
-} from '@/lib/types';
+import { BotFormData, DiscordBotRPCInfo, Screenshot } from '@/lib/types';
 import BotForm from '@/components/form/bot-form/BotForm';
+
+const keywords = [
+  '新增 Discord 伺服器',
+  'Discord 伺服器添加',
+  '創建 Discord 伺服器',
+  'Discord 伺服器列表',
+  '熱門 Discord 伺服器',
+  '免費 Discord 伺服器',
+  '人氣 Discord 伺服器',
+  'Discord 伺服器推薦',
+  '大型 Discord 伺服器',
+  '小型 Discord 伺服器',
+  '公開 Discord 伺服器',
+];
 
 const AddBotPage = () => {
   const handleCreate = async (data: BotFormData, screenshots: Screenshot[]) => {
@@ -33,17 +43,7 @@ const AddBotPage = () => {
       );
     }
 
-    const botInfoRes = await fetch(
-      `https://dchub.mantou.dev/member/${client_id}`,
-    );
-
-    if (!res.ok) {
-      throw new Error(`找不到此 Bot 的資訊 (status: ${res.status})`);
-    }
-
     const rpcData: DiscordBotRPCInfo = await res.json();
-
-    const botInfoData: BotInfo = await botInfoRes.json();
 
     const commandPayload = data.commands.map(cmd => ({
       name: cmd.name,
@@ -55,8 +55,6 @@ const AddBotPage = () => {
 
     const icon = `https://cdn.discordapp.com/app-icons/${client_id}/${rpcData.icon}.png`;
 
-    const devIds = data.developers.map(dev => dev.name);
-
     const botData: BotWithRelationsInput = {
       id: client_id,
       name: data.botName,
@@ -67,18 +65,18 @@ const AddBotPage = () => {
       users: 0,
       upvotes: 0,
       icon: icon,
-      banner: botInfoData.banner_url || null,
+      banner: null,
       featured: false,
       createdAt: new Date(),
       prefix: data.botPrefix,
-      developers: devIds.map(dev => ({ id: dev })),
+      developers: data.developers.map(dev => ({ id: dev.name })),
       website: data.botWebsite || null,
       VoteNotificationURL: data.webhook_url || '',
       secret: data.secret || '',
       status: 'pending',
       inviteUrl: data.botInvite,
       supportServer: data.botSupport || null,
-      verified: rpcData.is_verified,
+      verified: false,
       features: [],
       screenshots: screenshots.map(s => s.url),
       commands: commandPayload,
@@ -89,11 +87,47 @@ const AddBotPage = () => {
       subject: '已收到審核請求',
       teaser: `${data.botName} 審核請求`,
       content: `感謝您的申請！我們已收到您的審核請求，通常會在 1～2 個工作天內完成審核。\n審核結果將會同樣於此收件匣通知您，請定時確認以免影響自身權益。\n如審核後的一段時間都仍未收到回覆，請至支援群組開單詢問。`,
-      userIds: devIds,
     });
   };
 
-  return <BotForm mode="create" onSubmit={handleCreate} />;
+  return (
+    <>
+      <Head>
+        <title>新增機器人 | Discord機器人列表 - DiscordHubs</title>
+        <meta
+          name="description"
+          content="DiscordHubs是最佳的 Discord 中文機器人和機器人列表平台，你可以在此新增你的機器人，讓你的機器人得到宣傳和管理，快速建立專屬的社群空間。"
+        />
+        <meta name="keywords" content={keywords.join('，')} />
+        <meta name="author" content="DiscordHubs 團隊" />
+        <link rel="author" href="https://dchubs.org" />
+        <meta
+          property="og:title"
+          content="新增機器人 | Discord機器人列表 - DiscordHubs"
+        />
+        <meta
+          property="og:description"
+          content="DiscordHubs是最佳的 Discord 中文機器人和機器人列表平台，你可以在此新增你的機器人，讓你的機器人得到宣傳和管理，快速建立專屬的社群空間。"
+        />
+        <meta property="og:url" content="https://dchubs.org" />
+        <meta property="og:site_name" content="DiscordHubs" />
+        <meta
+          property="og:image"
+          content="https://dchubs.org/DCHUSB_banner.png"
+        />
+        <meta property="og:image:width" content="1012" />
+        <meta property="og:image:height" content="392" />
+        <meta
+          property="og:image:alt"
+          content="DiscordHubs Discord伺服器及機器人列表"
+        />
+        <meta property="og:locale" content="zh-TW" />
+        <meta property="og:type" content="website" />
+        <link rel="icon" href="https://dchubs.org/dchub.ico" />
+      </Head>
+      <BotForm mode="create" onSubmit={handleCreate} />
+    </>
+  );
 };
 
 export default AddBotPage;
