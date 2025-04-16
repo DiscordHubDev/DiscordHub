147c4b527563474af102c726224f302d8dd74ab3
victorydawn <setkillthe1@gmail.com>
2025-04-16 23:02:40 +0800

    關閉電腦端外面顯示的機器人banner

--- components/bot-card.tsx
+++ components/bot-card.tsx
@@ -168,7 +168,7 @@ export default function BotCard({ bot }: BotCardProps) {
           </div>
 
           {/* Bot Banner (desktop) */}
-          {bot.banner && (
+          {/* {bot.banner && (
             <div className="hidden md:block w-48 h-auto bg-[#36393f] flex-shrink-0">
               <img
                 src={bot.banner || '/placeholder.svg'}
@@ -176,7 +176,7 @@ export default function BotCard({ bot }: BotCardProps) {
                 className="w-full h-full object-cover"
               />
             </div>
-          )}
+          )} */}
         </div>
       </div>
     </Link>
