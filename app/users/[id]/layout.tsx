05b6d0f3da7fb93d5f90ff2b37f26fd866e856fa
victorydawn <setkillthe1@gmail.com>
2025-04-18 15:22:06 +0800

    刪除proflie和user的seo

--- app/users/[id]/layout.tsx
+++ app/users/[id]/layout.tsx
@@ -9,53 +9,53 @@ export async function generateMetadata({
 }: {
   params: Promise<{ id: string }>;
 }): Promise<Metadata> {
-  const { id } = await params;
-  const user = await getUserById(id);
-  const ogImages = [];
+  // const { id } = await params;
+  // const user = await getUserById(id);
+  // const ogImages = [];
 
-  if (user?.avatar) {
-    ogImages.push({
-      url: user.avatar,
-      width: 80,
-      height: 80,
-      alt: 'user-icon',
-    });
-  } else {
-    ogImages.push({
-      url: 'https://cdn.discordapp.com/embed/avatars/0.png',
-      width: 80,
-      height: 80,
-      alt: 'user-icon',
-    });
-  }
+  // if (user?.avatar) {
+  //   ogImages.push({
+  //     url: user.avatar,
+  //     width: 80,
+  //     height: 80,
+  //     alt: 'user-icon',
+  //   });
+  // } else {
+  //   ogImages.push({
+  //     url: 'https://cdn.discordapp.com/embed/avatars/0.png',
+  //     width: 80,
+  //     height: 80,
+  //     alt: 'user-icon',
+  //   });
+  // }
 
-  if (user?.banner) {
-    ogImages.push({
-      url: user.banner,
-      width: 960,
-      height: 540,
-      alt: 'user-banner',
-    });
-  }
-  if (!user) {
-    return { title: '用戶未找到 | DiscordHubs' };
-  }
+  // if (user?.banner) {
+  //   ogImages.push({
+  //     url: user.banner,
+  //     width: 960,
+  //     height: 540,
+  //     alt: 'user-banner',
+  //   });
+  // }
+  // if (!user) {
+  //   return { title: '用戶未找到 | DiscordHubs' };
+  // }
 
   return {
-    title: `${user.username} | DiscordHubs`,
-    description: `${user.username} 的個人頁面`,
-    icons: {
-      icon: '/favicon.ico',
-    },
-    openGraph: {
-      title: `${user.username} 的個人頁面`,
-      description: `DiscordHubs是最佳的 Discord 中文伺服器和機器人列表平台，你可以在此管理你的伺服器和機器人，讓你的伺服器得到宣傳和管理，編輯你的個人資料和管理收藏。`,
-      url: 'https://dchubs.org',
-      siteName: 'DiscordHubs',
-      images: ogImages,
-      locale: 'zh-TW',
-      type: 'website',
-    },
+    // title: `${user.username} | DiscordHubs`,
+    // description: `${user.username} 的個人頁面`,
+    // icons: {
+    //   icon: '/favicon.ico',
+    // },
+    // openGraph: {
+    //   title: `${user.username} 的個人頁面`,
+    //   description: `DiscordHubs是最佳的 Discord 中文伺服器和機器人列表平台，你可以在此管理你的伺服器和機器人，讓你的伺服器得到宣傳和管理，編輯你的個人資料和管理收藏。`,
+    //   url: 'https://dchubs.org',
+    //   siteName: 'DiscordHubs',
+    //   images: ogImages,
+    //   locale: 'zh-TW',
+    //   type: 'website',
+    // },
   };
 }
 
