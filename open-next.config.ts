9bf00d0b58a736c18e833912667c22b34d03f3ec
Mantouisyummy <51238168+Mantouisyummy@users.noreply.github.com>
2025-04-22 11:54:07 +0800

    Update open-next.config.ts

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,20 +1,3 @@
-// opennext.config.ts
-import type { Config } from "@opennextjs/cloudflare";
+import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 
-const config: Config = {
-  overrides: {
-    wrapper: "cloudflare-node",
-    converter: "edge",
-    proxyExternalRequest: "fetch",
-    incrementalCache: {
-      type: "dummy"
-    },
-    tagCache: "dummy",
-    queue: "dummy"
-  },
-  edgeExternals: ["node:crypto"]
-};
-
-export default config;
-
-
+export default defineCloudflareConfig();
