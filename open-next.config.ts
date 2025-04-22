a253a02f614f2d6c172923a39973123cdfca2b9b
Mantouisyummy <51238168+Mantouisyummy@users.noreply.github.com>
2025-04-22 13:54:37 +0800

    Update open-next.config.ts

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,9 +1,14 @@
 // opennext.config.ts
 export default {
   default: {
-    runtime: "edge",
-    cloudflare: {
-      pages: true
+    override: {
+      wrapper: "cloudflare-node",
+      converter: "edge",
+      proxyExternalRequest: "fetch",
+      incrementalCache: "dummy", // or a function
+      tagCache: "dummy",
+      queue: "dummy", // or "direct" or a function
     }
-  }
+  },
+  edgeExternals: ["node:crypto"]
 };
